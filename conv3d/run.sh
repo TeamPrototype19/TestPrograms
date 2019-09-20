b8186ee5668ffc716a2c0d9499ca87fc4f809064
Deokhwan,Kim <thekwan@naver.com>
2019-09-20 23:57:12 +0900

    Add test config file and parser.   + conv1, conv2 vector is verified.

--- conv3d/run.sh
+++ conv3d/run.sh
@@ -1,2 +1,3 @@
-./conv3d -i tv/tv_conv1_i.dat -w tv/tv_conv1_w.dat -b tv/tv_conv1_b.dat -o tv/tv_conv1_o.dat 
+./conv3d -i tv/tv_conv1_i.dat -w tv/tv_conv1_w.dat -b tv/tv_conv1_b.dat -o tv/tv_conv1_o.dat -c tv/tv_conv1_cfg.txt
+./conv3d -i tv/tv_conv2_i.dat -w tv/tv_conv2_w.dat -b tv/tv_conv2_b.dat -o tv/tv_conv2_o.dat -c tv/tv_conv2_cfg.txt
 #gdb --args ./conv3d -i tv/tv_conv1_i.dat -w tv/tv_conv1_w.dat -b tv/tv_conv1_b.dat -o tv/tv_conv1_o.dat 
--- conv3d/src/main.cc
+++ conv3d/src/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <unistd.h>
 #include <cstring>
 #include <cmath>
@@ -16,6 +17,14 @@ void check_sqnr( float *ref, float *out, int out_num, float &sqnr ) {
         float noise = (*ref++) - (*out++);
         spower += signal * signal;
         npower += noise * noise;
+        
+#if 0
+        if( fabs(noise) > 0.000001 ) {
+            std::cout << "ref = " << *(ref-1) \
+                      << "\tout = " << *(out-1) \
+                      << "\tdiff = " << noise << std::endl;
+        }
+#endif
     }
 
     sqnr = 10 * log10( spower / npower );
@@ -23,6 +32,71 @@ void check_sqnr( float *ref, float *out, int out_num, float &sqnr ) {
     //std::cout << "npower = " << npower << "\n";
 
     return;
+}
+
+int readConfigFile( ConvInfo& cinfo, std::string filename ) {
+    /* File open
+     */
+    std::ifstream ifs;
+    ifs.open(filename.c_str(), std::ios::in );
+
+    if( ! ifs.is_open() ) {
+        std::cout << "[ERROR] File open error: " << filename << std::endl;
+        return -1;
+    }
+
+    std::string line, key;
+    int value;
+    while( std::getline( ifs, line ) ) {
+        std::istringstream iss( line );
+
+        iss >> key;
+        iss >> value;
+
+        if( key.compare("ifm.size.n") == 0 ) {
+            cinfo.ifmDim[0] = value; 
+        }
+        else if( key.compare("ifm.size.c") == 0 ) {
+            cinfo.ifmDim[1] = value; 
+        }
+        else if( key.compare("ifm.size.h") == 0 ) {
+            cinfo.ifmDim[2] = value; 
+        }
+        else if( key.compare("ifm.size.w") == 0 ) {
+            cinfo.ifmDim[3] = value; 
+        }
+        else if( key.compare("kernel.size.w") == 0 ) {
+            cinfo.kernel_size_w = value;
+        }
+        else if( key.compare("kernel.size.h") == 0 ) {
+            cinfo.kernel_size_h = value;
+        }
+        else if( key.compare("stride.size.w") == 0 ) {
+            cinfo.stride_size_w = value;
+        }
+        else if( key.compare("stride.size.h") == 0 ) {
+            cinfo.stride_size_h = value;
+        }
+        else if( key.compare("pad.size.w") == 0 ) {
+            cinfo.pad_size_w = value;
+        }
+        else if( key.compare("pad.size.h") == 0 ) {
+            cinfo.pad_size_h = value;
+        }
+        else if( key.compare("group_num") == 0 ) {
+            cinfo.group_num = value;
+        }
+        else if( key.compare("output_num") == 0 ) {
+            cinfo.output_num = value;
+        }
+        else {
+            // invalid parameter !!
+            std::cerr << "[ERROR] invalid config parameter: " << key << std::endl;
+            return -1;
+        }
+    }
+
+    return 0;
 }
 
 int readBinaryData(char* &buf, std::string filename) {
@@ -55,11 +129,11 @@ int readBinaryData(char* &buf, std::string filename) {
 
 int main(int argc, char **argv) {
 	char option;
-	const char *optstring = "i:o:w:b:o:";
+	const char *optstring = "i:o:w:b:c:";
 
-	if( argc != 9 ) {
+	if( argc != 11 ) {
         std::cerr << "Not enough inputs." << std::endl;
-        std::cerr << "nnc -i [IFM file] -w [Weight file] -b [bias file] -o [OFM file]" << std::endl;
+        std::cerr << "nnc -i [IFM file] -w [Weight file] -b [bias file] -o [OFM file] -c [config file]" << std::endl;
 		return -1;
 	}
 
@@ -67,6 +141,10 @@ int main(int argc, char **argv) {
 	std::string weightFileName = "weight.dat";
 	std::string biasFileName   = "bias.dat";
 	std::string ofmFileName    = "ofm.dat";
+    std::string configFileName = "config.txt";
+    int bs_n, bs_c, bs_h, bs_w;
+    int kernel_size, stride_size, pad_size;
+    int output_num, group_num;
 
     while( -1 != (option = getopt(argc, argv, optstring))) {
 		switch(option) {
@@ -78,6 +156,8 @@ int main(int argc, char **argv) {
 						break;
 			case 'o':	ofmFileName = optarg;
 						break;
+			case 'c':	configFileName = optarg;
+						break;
 		}
 	}
 
@@ -87,6 +167,7 @@ int main(int argc, char **argv) {
     std::cout << "Weight file name = " << weightFileName << std::endl;
     std::cout << "Bias file name   = " << biasFileName << std::endl;
     std::cout << "OFM file name    = " << ofmFileName << std::endl;
+    std::cout << "config file name = " << configFileName << std::endl;
 
     char *ibuf, *wbuf, *bbuf, *obuf, *robuf;
 
@@ -118,14 +199,11 @@ int main(int argc, char **argv) {
     /* construct conv test date information 
      */
     ConvInfo cinfo;
-    cinfo.ifmDim[0] = 1;
-    cinfo.ifmDim[1] = 1; 
-    cinfo.ifmDim[2] = 28;
-    cinfo.ifmDim[3] = 28;
-    cinfo.kernel_size_w = 5; cinfo.kernel_size_h = 5;
-    cinfo.stride_size_w = 1; cinfo.stride_size_h = 1;
-    cinfo.pad_size_w    = 0; cinfo.pad_size_h    = 0;
-    cinfo.output_num = 20;
+    if( readConfigFile( cinfo, configFileName ) != 0 ) {
+        std::cerr << "[Error] fail to process config file!" << std::endl;
+        return 0;
+    }
+
 
     /* Run conv operation test code 
      */
@@ -143,7 +221,7 @@ int main(int argc, char **argv) {
     float Sqnr;
     check_sqnr( (float*)robuf, (float*)obuf, obuf_size/sizeof(float), Sqnr );
 
-    std::cout << "SQNR = " << Sqnr << std::endl;
+    std::cout << "SQNR = " << Sqnr << " (dB)" << std::endl;
 
     /* Free allocated buffers
      */
